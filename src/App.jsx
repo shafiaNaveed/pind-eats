fcaf1138f80d4bf05c2e1fbf116e9578c7c5ea96
Shafia Naveed <navidkhan5272@gmail.com>
2026-04-20 20:57:51 +0500

    bg

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,16 @@ import Gifts from './Components/Gifts'
 import Section from './Components/Section'
 import Main from './Components/Main'
 import Offer from './Components/Offer'
+import bg from '/src/assets/imgs/bg.png'
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <div className='relative overflow-hidden'>
+        <div className='absolute top-0 right-0 w-1/2 lg:width-[50%] h-full bg-no-repeat bg-cover bg-right-top -z-10 ' style={{backgroundImage: `url(${bg})`,backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', backgroundPosition: 'right top'}}></div>
+        <Navbar/>
       <Hero/>
+      </div>
       <Featured/>
       <Gifts/>
       <Section/>
